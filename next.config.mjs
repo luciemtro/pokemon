27dc6c21ea5775cb5fc58a2ddb62e2f891597379
Lucie Monteir <lucie.mtro@gmail.com>
2025-02-09 22:42:09 +0100

    refactor(config): remove unused Next.js configuration options and update dependencies for improved security

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
-  swcMinify: true,
-  onDemandEntries: {
-    maxInactiveAge: 25 * 1000,
-    pagesBufferLength: 5,
-  },
 };
 
 export default nextConfig;
