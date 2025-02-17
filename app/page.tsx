8fd1d51ed74109f7e18e1499f90e3f9ca3a0c1cf
Lucie Monteir <lucie.mtro@gmail.com>
2025-02-17 13:57:51 +0100

    feat(home): integrate carousel component with image array for enhanced homepage display

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
+import Carousel from "@/components/Carousel";
+
+const images = [
+  "/images/pokemon/bulbizare.webp",
+  "/images/pokemon/carapuce.webp",
+  "/images/pokemon/dresseur.webp",
+  "/images/pokemon/evoli.webp",
+  "/images/pokemon/gardevoir.webp",
+  "/images/pokemon/lucario.webp",
+  "/images/pokemon/mewtwo.webp",
+  "/images/pokemon/pikachu.webp",
+  "/images/pokemon/ronflex.webp",
+  "/images/pokemon/amphinobi.webp",
+  "/images/pokemon/spectrum.webp",
+];
+
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="relative w-full h-full flex items-center justify-center bg-[url('/images/pokeball.webp')] bg-cover bg-center">
+      {/* Gradient en cercle */}
+      <div className="absolute inset-0 bg-radial-gradient pointer-events-none"></div>
+
+      <Carousel images={images} />
+    </main>
+  );
 }
