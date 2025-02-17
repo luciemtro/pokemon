d803726eac96fd30b8dd37e119533b722eac41a3
Lucie Monteir <lucie.mtro@gmail.com>
2025-02-17 13:54:00 +0100

    feat(home): update homepage layout with images and new button for card discovery

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,39 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="">
-      <article>
-        <h1 className="">
-          Bienvenue sur notre site de vente de cartes Pokémon
-        </h1>
-        <p className="">
-          Vous trouverez ici un large choix de cartes Pokémon, de la première
-          génération à la dernière.
-        </p>
+    <main>
+      <article className="">
+        <header></header>
+        <section className="flex justify-between items-center">
+          <picture>
+            <Image
+              src="/images/pokemon-banner.jpg"
+              alt="Cartes Pokémon"
+              width={300}
+              height={300}
+              priority
+            />
+          </picture>
+          <div className="text-center">
+            <h1>Bienvenue sur notre Pokémon Store</h1>
+            <p>
+              Vous trouverez ici un large choix de cartes Pokémon, de la
+              première génération à la dernière.
+            </p>
+            <button className="btn-slanted-yellow">
+              Découvrez nos cartes Pokémon
+            </button>
+          </div>
+          <picture>
+            <Image
+              src="/images/pokemon-collection.jpg"
+              alt="Collection de cartes"
+              width={300}
+              height={300}
+            />
+          </picture>
+        </section>
       </article>
     </main>
   );
